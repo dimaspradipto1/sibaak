58fc91d03ee432a415921a204faae0558467296e
dimas pradipto <dpradipto8@gmail.com>
2026-01-07 19:42:24 +0700

    feat: drive sop akademik dan wasdalbon

--- app/Http/Controllers/SopAkademikController.php
+++ app/Http/Controllers/SopAkademikController.php
@@ -36,37 +36,37 @@ class SopAkademikController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(SopAkademikRequest $request)
-    {
-        try {
-            $data = $request->validated();
-            $data['users_id'] = Auth::id();
+    // public function store(SopAkademikRequest $request)
+    // {
+    //     try {
+    //         $data = $request->validated();
+    //         $data['users_id'] = Auth::id();
 
-            if ($request->hasFile('file')) {
-                $file = $request->file('file');
-                $filename = $file->getClientOriginalName();
-                $path = $file->storeAs('sopakademik', $filename, 'public');
-                $data['file'] = 'storage/' . $path;
-            }
+    //         if ($request->hasFile('file')) {
+    //             $file = $request->file('file');
+    //             $filename = $file->getClientOriginalName();
+    //             $path = $file->storeAs('sopakademik', $filename, 'public');
+    //             $data['file'] = 'storage/' . $path;
+    //         }
 
-            SopAkademik::create($data);
+    //         SopAkademik::create($data);
 
-            Alert::success('Berhasil', 'Data Berhasil Ditambahkan')
-                ->autoclose(3000)
-                ->toToast()
-                ->timerProgressBar()
-                ->iconHtml('<i class="fa fa-check-circle"></i>');
-            return redirect()->route('sopakademik.index');
-        } catch (\Throwable $th) {
-            Log::error('Error storing SOP Akademik: ' . $th->getMessage());
-            Alert::error('Gagal', 'Data Gagal Ditambahkan')
-                ->autoclose(3000)
-                ->toToast()
-                ->timerProgressBar()
-                ->iconHtml('<i class="fa fa-times-circle"></i>');
-            return redirect()->back()->withInput();
-        }
-    }
+    //         Alert::success('Berhasil', 'Data Berhasil Ditambahkan')
+    //             ->autoclose(3000)
+    //             ->toToast()
+    //             ->timerProgressBar()
+    //             ->iconHtml('<i class="fa fa-check-circle"></i>');
+    //         return redirect()->route('sopakademik.index');
+    //     } catch (\Throwable $th) {
+    //         Log::error('Error storing SOP Akademik: ' . $th->getMessage());
+    //         Alert::error('Gagal', 'Data Gagal Ditambahkan')
+    //             ->autoclose(3000)
+    //             ->toToast()
+    //             ->timerProgressBar()
+    //             ->iconHtml('<i class="fa fa-times-circle"></i>');
+    //         return redirect()->back()->withInput();
+    //     }
+    // }
 
     // TODO: SUBMIT TO GOOGLE DRIVE
     // public function store(SopAkademikRequest $request)
@@ -194,6 +194,101 @@ class SopAkademikController extends Controller
     //     ]);
     // }
     // TODO: END SUBMIT TO GOOGLE DRIVE
+
+    //? fix code
+    public function store(SopAkademikRequest $request)
+    {
+        try {
+            $data = $request->validated();
+            $data['users_id'] = Auth::id();
+
+            if ($request->hasFile('file')) {
+                $uploaded = $request->file('file');
+                $safeOriginal = preg_replace('/[^A-Za-z0-9\.\-_ ]/', '', $uploaded->getClientOriginalName());
+                $filename = $safeOriginal;
+
+                // Upload via Google Drive API
+                $service = $this->driveService();
+                $sopakademikFolderId = env('GOOGLE_DRIVE_SOP_FOLDER_ID');
+
+                if (!$sopakademikFolderId) {
+                    throw new \Exception('GOOGLE_DRIVE_SOP_FOLDER_ID tidak ditemukan di .env');
+                }
+
+                $fileMetadata = new \Google\Service\Drive\DriveFile([
+                    'name' => $filename,
+                    'parents' => [$sopakademikFolderId],
+                ]);
+
+                $content = File::get($uploaded->getRealPath());
+
+                $file = $service->files->create($fileMetadata, [
+                    'data' => $content,
+                    'uploadType' => 'multipart',
+                    'fields' => 'id',
+                ]);
+
+                $fileId = $file->getId();
+
+                // Jadikan public
+                $this->makeDriveFilePublic($fileId);
+
+                // Simpan link (tanpa spasi!)
+                $data['file'] = "https://drive.google.com/file/d/{$fileId}/view";
+            }
+
+            SopAkademik::create($data);
+
+            Alert::success('Berhasil', 'SOP Akademik Berhasil Ditambahkan')
+                ->autoclose(4000)
+                ->toToast()
+                ->timerProgressBar()
+                ->iconHtml('<i class="fa fa-check-circle"></i>');
+
+            return redirect()->route('sopakademik.index');
+
+        } catch (\Throwable $th) {
+            Log::error('Error storing SOP Akademik: ' . $th->getMessage());
+
+            Alert::error('Gagal', 'Gagal menambahkan data: ' . $th->getMessage())
+                ->autoclose(5000)
+                ->toToast()
+                ->timerProgressBar()
+                ->iconHtml('<i class="fa fa-times-circle"></i>');
+
+            return redirect()->back()->withInput();
+        }
+    }
+
+    private function driveService(): GoogleDrive
+    {
+        $client = new GoogleClient();
+        $client->setClientId(env('GOOGLE_DRIVE_CLIENT_ID'));
+        $client->setClientSecret(env('GOOGLE_DRIVE_CLIENT_SECRET'));
+
+        $refreshToken = env('GOOGLE_DRIVE_REFRESH_TOKEN');
+        if (!$refreshToken) {
+            throw new \Exception('Refresh token tidak ditemukan di .env');
+        }
+
+        $client->fetchAccessTokenWithRefreshToken($refreshToken);
+        return new GoogleDrive($client);
+    }
+
+    private function makeDriveFilePublic(string $fileId): void
+    {
+        $service = $this->driveService();
+        $permission = new Permission([
+            'type' => 'anyone',
+            'role' => 'reader',
+        ]);
+
+        $service->permissions->create($fileId, $permission, [
+            'fields' => 'id',
+            'supportsAllDrives' => true,
+        ]);
+    }
+    //? end fix code drive
 
     /**
      * Display the specified resource.
--- app/Http/Controllers/WasdalbinController.php
+++ app/Http/Controllers/WasdalbinController.php
@@ -69,6 +69,133 @@ class WasdalbinController extends Controller
     // }
 
      // TODO: SUBMIT TO GOOGLE DRIVE
+    // public function store(WasdalbinRequest $request)
+    // {
+    //     try {
+    //         $data = $request->validated();
+    //         $data['users_id'] = Auth::id();
+
+    //         if ($request->hasFile('file')) {
+    //             $uploaded = $request->file('file');
+
+    //             // Mengambil nama asli file dan memastikan nama file aman
+    //             $safeOriginal = preg_replace('/[^A-Za-z0-9\.\-_ ]/', '', $uploaded->getClientOriginalName());  // Menghapus karakter yang tidak diizinkan
+
+    //             // Menggunakan nama file yang sudah aman tanpa underscore
+    //             $filename = $safeOriginal;
+
+    //             // Path file di Google Drive (tanpa bagian user ID)
+    //             $drivePath = 'Wasdalbin/' . $filename;
+
+    //             try {
+    //                 // 1) Upload ke Google Drive
+    //                 Storage::disk('google')->put($drivePath, File::get($uploaded->getRealPath()));
+
+    //                 // 2) Ambil fileId (dibutuhkan untuk set permission public)
+    //                 $fileId = $this->findDriveFileIdByNameInRootFolder($filename);
+
+    //                 if (!$fileId) {
+    //                     throw new \Exception("Upload berhasil tapi fileId tidak ditemukan. Pastikan GOOGLE_DRIVE_FOLDER berisi Folder ID yang benar.");
+    //                 }
+
+    //                 // 3) Jadikan public (anyone with link)
+    //                 $this->makeDriveFilePublic($fileId);
+
+    //                 // 4) Simpan link share ke DB
+    //                 $data['file'] = "https://drive.google.com/file/d/{$fileId}/view";
+    //             } catch (\Throwable $e) {
+    //                 Log::error('Google Drive Error (Fallback to Local): ' . $e->getMessage());
+
+    //                 //fallback local
+    //                 $localPath = $uploaded->storeAs('wasdalbin', $filename, 'public');
+    //                 $data['file'] = 'storage/' . $localPath;
+    //             }
+    //         }
+
+    //         Wasdalbin::create($data);
+
+    //         Alert::success('Berhasil', 'Data Berhasil Ditambahkan')
+    //             ->autoclose(4000)
+    //             ->toToast()
+    //             ->timerProgressBar()
+    //             ->iconHtml('<i class="fa fa-check-circle"></i>');
+
+    //         return redirect()->route('wasdalbin.index');
+    //     } catch (\Throwable $th) {
+    //         Log::error('Error storing Wasdalbin: ' . $th->getMessage());
+
+    //         Alert::error('Gagal', 'Data Gagal Ditambahkan: ' . $th->getMessage())
+    //             ->autoclose(5000)
+    //             ->toToast()
+    //             ->timerProgressBar()
+    //             ->iconHtml('<i class="fa fa-times-circle"></i>');
+
+    //         return redirect()->back()->withInput();
+    //     }
+    // }
+
+    /**
+     * Google Drive service dari refresh token.
+     */
+    // private function driveService(): GoogleDrive
+    // {
+    //     $client = new GoogleClient();
+    //     $client->setClientId(env('GOOGLE_DRIVE_CLIENT_ID'));
+    //     $client->setClientSecret(env('GOOGLE_DRIVE_CLIENT_SECRET'));
+    //     $client->refreshToken(env('GOOGLE_DRIVE_REFRESH_TOKEN'));
+
+    //     return new GoogleDrive($client);
+    // }
+
+    /**
+     * Cari fileId berdasarkan nama file di folder root yang dipakai disk google (GOOGLE_DRIVE_FOLDER).
+     * GOOGLE_DRIVE_FOLDER kamu harus berisi Folder ID.
+     */
+    // private function findDriveFileIdByNameInRootFolder(string $filename): ?string
+    // {
+    //     $service = $this->driveService();
+
+    //     $rootFolderId = env('GOOGLE_DRIVE_FOLDER'); // ini folderId root kamu
+    //     if (!$rootFolderId) {
+    //         return null;
+    //     }
+
+    //     $escaped = str_replace("'", "\\'", $filename);
+    //     $q = "name = '{$escaped}' and trashed = false and '{$rootFolderId}' in parents";
+
+    //     $list = $service->files->listFiles([
+    //         'q' => $q,
+    //         'fields' => 'files(id,name,createdTime)',
+    //         'orderBy' => 'createdTime desc',
+    //         'pageSize' => 1,
+    //         'supportsAllDrives' => true,      // aman kalau folder ada di shared drive
+    //         'includeItemsFromAllDrives' => true,
+    //     ]);
+
+    //     $files = $list->getFiles();
+    //     return count($files) ? $files[0]->getId() : null;
+    // }
+
+    /**
+     * Set permission: anyone with link (reader)
+     */
+    // private function makeDriveFilePublic(string $fileId): void
+    // {
+    //     $service = $this->driveService();
+
+    //     $permission = new Permission([
+    //         'type' => 'anyone',
+    //         'role' => 'reader',
+    //     ]);
+
+    //     $service->permissions->create($fileId, $permission, [
+    //         'fields' => 'id',
+    //         'supportsAllDrives' => true,
+    //     ]);
+    // }
+    // TODO: END SUBMIT TO GOOGLE DRIVE
+
+    //? fix code
     public function store(WasdalbinRequest $request)
     {
         try {
@@ -77,54 +204,53 @@ class WasdalbinController extends Controller
 
             if ($request->hasFile('file')) {
                 $uploaded = $request->file('file');
-
-                // Mengambil nama asli file dan memastikan nama file aman
-                $safeOriginal = preg_replace('/[^A-Za-z0-9\.\-_ ]/', '', $uploaded->getClientOriginalName());  // Menghapus karakter yang tidak diizinkan
-
-                // Menggunakan nama file yang sudah aman tanpa underscore
+                $safeOriginal = preg_replace('/[^A-Za-z0-9\.\-_ ]/', '', $uploaded->getClientOriginalName());
                 $filename = $safeOriginal;
 
-                // Path file di Google Drive (tanpa bagian user ID)
-                $drivePath = 'Wasdalbin/' . $filename;
+                // Upload via Google Drive API
+                $service = $this->driveService();
+                $wasdalbinFolderId = env('GOOGLE_DRIVE_WASDALBIN_FOLDER_ID');
 
-                try {
-                    // 1) Upload ke Google Drive
-                    Storage::disk('google')->put($drivePath, File::get($uploaded->getRealPath()));
-
-                    // 2) Ambil fileId (dibutuhkan untuk set permission public)
-                    $fileId = $this->findDriveFileIdByNameInRootFolder($filename);
-
-                    if (!$fileId) {
-                        throw new \Exception("Upload berhasil tapi fileId tidak ditemukan. Pastikan GOOGLE_DRIVE_FOLDER berisi Folder ID yang benar.");
-                    }
-
-                    // 3) Jadikan public (anyone with link)
-                    $this->makeDriveFilePublic($fileId);
-
-                    // 4) Simpan link share ke DB
-                    $data['file'] = "https://drive.google.com/file/d/{$fileId}/view";
-                } catch (\Throwable $e) {
-                    Log::error('Google Drive Error (Fallback to Local): ' . $e->getMessage());
-
-                    //fallback local
-                    $localPath = $uploaded->storeAs('wasdalbin', $filename, 'public');
-                    $data['file'] = 'storage/' . $localPath;
+                if (!$wasdalbinFolderId) {
+                    throw new \Exception('GOOGLE_DRIVE_WASDALBIN_FOLDER_ID tidak ditemukan di .env');
                 }
+
+                $fileMetadata = new \Google\Service\Drive\DriveFile([
+                    'name' => $filename,
+                    'parents' => [$wasdalbinFolderId],
+                ]);
+
+                $content = File::get($uploaded->getRealPath());
+
+                $file = $service->files->create($fileMetadata, [
+                    'data' => $content,
+                    'uploadType' => 'multipart',
+                    'fields' => 'id',
+                ]);
+
+                $fileId = $file->getId();
+
+                // Jadikan public
+                $this->makeDriveFilePublic($fileId);
+
+                // Simpan link (tanpa spasi!)
+                $data['file'] = "https://drive.google.com/file/d/{$fileId}/view";
             }
 
             Wasdalbin::create($data);
 
-            Alert::success('Berhasil', 'Data Berhasil Ditambahkan')
+            Alert::success('Berhasil', 'Wasdalbin Berhasil Ditambahkan')
                 ->autoclose(4000)
                 ->toToast()
                 ->timerProgressBar()
                 ->iconHtml('<i class="fa fa-check-circle"></i>');
 
             return redirect()->route('wasdalbin.index');
+
         } catch (\Throwable $th) {
             Log::error('Error storing Wasdalbin: ' . $th->getMessage());
 
-            Alert::error('Gagal', 'Data Gagal Ditambahkan: ' . $th->getMessage())
+            Alert::error('Gagal', 'Gagal menambahkan data: ' . $th->getMessage())
                 ->autoclose(5000)
                 ->toToast()
                 ->timerProgressBar()
@@ -134,55 +260,24 @@ class WasdalbinController extends Controller
         }
     }
 
-    /**
-     * Google Drive service dari refresh token.
-     */
     private function driveService(): GoogleDrive
     {
         $client = new GoogleClient();
         $client->setClientId(env('GOOGLE_DRIVE_CLIENT_ID'));
         $client->setClientSecret(env('GOOGLE_DRIVE_CLIENT_SECRET'));
-        $client->refreshToken(env('GOOGLE_DRIVE_REFRESH_TOKEN'));
 
+        $refreshToken = env('GOOGLE_DRIVE_REFRESH_TOKEN');
+        if (!$refreshToken) {
+            throw new \Exception('Refresh token tidak ditemukan di .env');
+        }
+
+        $client->fetchAccessTokenWithRefreshToken($refreshToken);
         return new GoogleDrive($client);
     }
 
-    /**
-     * Cari fileId berdasarkan nama file di folder root yang dipakai disk google (GOOGLE_DRIVE_FOLDER).
-     * GOOGLE_DRIVE_FOLDER kamu harus berisi Folder ID.
-     */
-    private function findDriveFileIdByNameInRootFolder(string $filename): ?string
-    {
-        $service = $this->driveService();
-
-        $rootFolderId = env('GOOGLE_DRIVE_FOLDER'); // ini folderId root kamu
-        if (!$rootFolderId) {
-            return null;
-        }
-
-        $escaped = str_replace("'", "\\'", $filename);
-        $q = "name = '{$escaped}' and trashed = false and '{$rootFolderId}' in parents";
-
-        $list = $service->files->listFiles([
-            'q' => $q,
-            'fields' => 'files(id,name,createdTime)',
-            'orderBy' => 'createdTime desc',
-            'pageSize' => 1,
-            'supportsAllDrives' => true,      // aman kalau folder ada di shared drive
-            'includeItemsFromAllDrives' => true,
-        ]);
-
-        $files = $list->getFiles();
-        return count($files) ? $files[0]->getId() : null;
-    }
-
-    /**
-     * Set permission: anyone with link (reader)
-     */
     private function makeDriveFilePublic(string $fileId): void
     {
         $service = $this->driveService();
-
         $permission = new Permission([
             'type' => 'anyone',
             'role' => 'reader',
@@ -193,7 +288,7 @@ class WasdalbinController extends Controller
             'supportsAllDrives' => true,
         ]);
     }
-    // TODO: END SUBMIT TO GOOGLE DRIVE
+    //? end fix code drive
 
     /**
      * Display the specified resource.
