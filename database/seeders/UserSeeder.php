c4f2b0f3f3d1ab3c765f6b91a245f7e32fdf620a
dimas pradipto <dpradipto8@gmail.com>
2026-04-22 18:18:23 +0700

    refactor: userseeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -76,8 +76,8 @@ class UserSeeder extends Seeder
                 'is_active' => true,
             ],
             [
-                'name' => 'TU FST',
-                'email' => 'fst@uis.ac.id',
+                'name' => 'M. Ansar',
+                'email' => 'm.ansar@uis.ac.id',
                 'password' => Hash::make('password'),
                 'role_id' => $roles['TATA USAHA FST'] ?? null,
                 'is_active' => true,
